0d1fa627c6c1905e9e2e3d850e6c95a570b44ee4
JixunMoe <Jixun.Moe@Gmail.com>
2014-03-23 14:49:20 +0000

    Automated Build Time

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #/bin/bash
-cat filter-header dev/* > my-china-list-build.txt
+touch filter-build-info
+echo -e "\n!-- Build at $(date)\n" > filter-build-info
+
+cat --squeeze-blank filter-header filter-build-info dev/* > my-china-list-build.txt
 # nodejs checksum.js my-china-list-build.txt
 python2 add_checksum.py my-china-list-build.txt
 echo Build Finish.
