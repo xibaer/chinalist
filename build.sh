4d0d6bcb1c5db860ff6c1237532b0820d495533f
JixunMoe <Jixun.Moe@Gmail.Com>
2014-05-19 23:07:06 +0100

    Fixes..?

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 touch filter-build-info
 echo -e "\n!-- Build at $(date)\n" > filter-build-info
 
+rm dev/*~
+rm dev/*.txt.bak
+
 cat --squeeze-blank filter-header filter-build-info dev/*.txt > my-china-list-build.txt
 # nodejs checksum.js my-china-list-build.txt
 python2 add_checksum.py my-china-list-build.txt
